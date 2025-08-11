583eef3126471dccdced81a62d375e2a15b844ca
konkam <guillaume.konkamking.work@gmail.com>
2025-08-11 14:39:54 +0200

    added code for credible band

--- scripts/get_CDF_BNPdensity.R
+++ scripts/get_CDF_BNPdensity.R
@@ -41,3 +41,50 @@ get_CDF_semi_BNPdensity = function(fit, xs = seq(-5,5, length.out = 100)){
   pmixnorm_vec_loop(xs = xs, means_list = fit$means, sigmas_list = fit$sigmas_filled, weights_list = fit$weights)
   
 }  
+
+cppFunction('NumericMatrix pmixnorm_matrix(NumericVector xs, 
+                                           Rcpp::List means_list, 
+                                           Rcpp::List sigmas_list, 
+                                           Rcpp::List weights_list) {
+  int nit = means_list.size(); 
+  int nx = xs.size();
+  NumericMatrix res(nit, nx);
+
+  for(int i = 0; i < nit; ++i) {
+    NumericVector mus = as<NumericVector>(means_list[i]);
+    NumericVector sigmas = as<NumericVector>(sigmas_list[i]);
+    NumericVector weights = as<NumericVector>(weights_list[i]);
+
+    for(int j = 0; j < mus.size(); ++j) {
+      for(int k = 0; k < nx; ++k) {
+        res(i, k) += R::pnorm(xs[k], mus[j], sigmas[j], 1, 0) * weights[j];
+      }
+    }
+  }
+
+  return res;
+}')
+
+get_CDF_with_quantiles_BNPdensity <- function(fit, xs = seq(-5, 5, length.out = 100), probs = c(0.025, 0.975)) {
+  # Fill sigmas if needed
+  if (is.null(fit$sigmas_filled)) {
+    fit$sigmas_filled <- fill_sigmas(fit)
+  }
+  
+  # Get matrix of CDF values: rows = iterations, cols = xs
+  cdf_matrix <- pmixnorm_matrix(xs = xs, 
+                                means_list = fit$means, 
+                                sigmas_list = fit$sigmas_filled, 
+                                weights_list = fit$weights)
+  
+  # Compute pointwise mean and quantiles
+  mean_cdf <- colMeans(cdf_matrix)
+  quantiles_cdf <- apply(cdf_matrix, 2, quantile, probs = probs)
+  
+  # Return as a data frame
+  result <- data.frame(xs = xs,
+                       mean = mean_cdf,
+                       lower = quantiles_cdf[1, ],
+                       upper = quantiles_cdf[2, ])
+  return(result)
+}
